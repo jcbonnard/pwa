a7b40079ebd21f11c64ee6e2b8629314dc5574dc
Jean-Christophe Bonnard <jeanchristophe.bonnard@infrabel.be>
2018-04-20 10:15:47 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule, MatCardModule  } from '@angular/material';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { 
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule 
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
@@ -14,8 +21,10 @@ import { ImgCardComponent } from './img-card/img-card.component';
   ],
   imports: [
     BrowserModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
     MatToolbarModule,
-    MatCardModule 
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
 
 class CatImage {
   message: string;
   api: string;
   fontsize: number;
+}
+
+class Button {
+  text: string;
+  disabled: boolean;
+  color: string;
 }
 
 @Component({
@@ -12,24 +19,33 @@ class CatImage {
   styleUrls: ['./img-card.component.css']
 })
 export class ImgCardComponent implements OnInit {
+
   private image: CatImage = {
     message: 'Progressive Web Cat',
     api: 'https://cataas.com/cat/says/',
     fontsize: 40
   };
 
-  constructor() { }
+  public button: Button = {
+    text: 'Give me another cat',
+    color: 'primary',
+    disabled: false
+  };
 
   public src: string;
 
+  constructor() { }
+  
   ngOnInit() {
-    this.generateSrc();
-  }
+    this.src = this.image.api + this.image.message + '?size=' + this.image.fontsize;
 
+    if (!navigator.onLine) {
+      this.button.text = 'Sorry, you\'re offline';
+      this.button.disabled = true;
+    }
+  }
+  
   public generateSrc(): void {
-    this.src = this.image.api + this.image.message + 
-      '?size=' + this.image.fontsize +
-      '&ts=' + Date.now();
+    this.src = this.src.replace(/\&ts=[\w]*/gi, '') + '&ts=' + Date.now();
   }
-
 }
